d3559d5a3fc144d1e78f3ca5cd91d72a00de8855
Anuj13599 <94560287+Anuj13599@users.noreply.github.com>
2023-07-20 12:41:40 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,46 +1,27 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import Header from "./components/Header";
-import Recipes from "./components/Recipes";
-import Axios from "axios";
+import Login from "./components/login";
+import RegistrationForm from "./components/Registration";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./components/Home";
 
 function App() {
-  const [search, setSearch] = useState("apple");
-  const [recipes, setRecipes] = useState([]);
-
-  const APP_ID = "c3fad4ac";
-  const APP_KEY = "8d59cf36ea941bb6edd588276fb3cf3e";
-
-  useEffect(() => {
-    getRecipes();
-  }, []);
-
-  const getRecipes = async () => {
-    const res = await Axios.get(
-      `https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY }`
-    );
-    setRecipes(res.data.hits);
-  };
-
-  const onInputChange = e => { 
-    setSearch(e.target.value);
-  };
-
-  const onSearchClick = () => {
-    getRecipes();
-  };
+  
   return (
+       
     <div className="App">
-      <Header
-        search={search}
-        onInputChange={onInputChange}
-        onSearchClick={onSearchClick}
-      />
-      <div className="container">
-        <Recipes recipes={recipes} />
-      </div>
-    </div>
-  );
+      <Routes>
+      <Route exact path="/"
+      element={<Login />}/>
+      <Route path="/registrationform"
+      element={<RegistrationForm/>} />
+      <Route path="/login"
+      element={<Login/>} /> 
+      <Route path="/homepage"
+      element={<Homepage />} />  
+      </Routes> 
+    </div>      
+  ); 
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
+
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Header = props => {
   const { search, onInputChange, onSearchClick } = props;
   return (
-    <div className="jumbotron">
+  <div className="jumbotron">
       <h1 className="display-1">
         <i className="material-icons brand-icon">fastfood</i> Food Recipe
       </h1>
+      <div className="Logout"><button>
+             <Link to="/login">LogOut</Link>
+               </button></div>
       <div className="input-group w-50 mx-auto">
         <input
           type="text"
